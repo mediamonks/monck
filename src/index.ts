2d9d4935276396776cb951d9cf8627f8e2347099
Arjan van Wijk <thanarie@gmail.com>
2022-01-05 17:12:00 +0100

    Export missing type

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export { createServer } from './createServer.js';
 export { getMockMiddleware as createMockMiddleware } from './getMockMiddleware.js';
+export type { RequestConfig } from './getMockMiddleware.js';
